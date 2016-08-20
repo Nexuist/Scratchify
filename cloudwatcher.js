85d0bec4486f010ce55a481986ff842a100585ad
Nexuist <nexuister@gmail.com>
2016-08-20 15:27:59 -0400

    Finished majority of architecture

--- cloudwatcher.js
+++ cloudwatcher.js
@@ -1,12 +1,15 @@
 var spotify = require("spotify-node-applescript");
 var https = require("https");
 
+/* Constants */
 var refreshInterval = 1000; // Milliseconds between refresh
 var projectID = "93077193";
+var opts = {
+	host: "scratch.mit.edu",
+	path: "/varserver/" + projectID
+};
 
-// __TODO__ check if you can use log-prefix to signal success and fail
-
-// Any console logs with have a timestamp prefix now
+/* Add timestamp prefix to console methods */
 require("log-prefix")(function() {
 	var d = new Date();
 	return require("util").format("[%d/%d/%d %d:%d:%d] %%s",
@@ -19,149 +22,86 @@ require("log-prefix")(function() {
 	);
 });
 
-function check() {
-	var opts = {
-		host: "scratch.mit.edu",
-		path: "/varserver/" + projectID
-	};
+/* Called whenever a spotify method is executed */
+function callback() {
+	spotify.getState(function(err, state) {
+		if (!err) {
+			console.log("Spotify response:", JSON.stringify(state));
+		}
+		else {
+			console.error("Spotify error:", state);
+		}
+	});
+}
+
+/* Retrieve and strip response JSON */
+function refresh() {
 	var request = https.get(opts, function(res) {
-		res.on("data", function (data) {
-			console.log(data);
+		var body = "";
+		res.on("data", function (chunk) {
+			body += chunk;
 		});
+		res.on("end", function() {
+			var failed = false;
+			var vars = {};
+			try {
+				var json = JSON.parse(body).variables;
+				json.forEach(function(obj) {
+					vars[obj.name.substring(2)] = obj.value;
+				});
+				failed = false;
+			}
+			catch (err) {
+				failed = true;
+				console.error("Couldn't parse response JSON:", err);
+			}
+			if (!failed) {
+				parse(vars);
+			}
+		});
+	});
+	request.on("error", function(err) {
+		console.error(err);
 	});
 	request.end();
 }
 
-setInterval(check, refreshInterval);
+/* Check if id or volume has changed and proxy that info to Spotify */
+function parse(vars) {
+	if (!parse.oldVars) {
+		parse.oldVars = vars;
+	}
+	if (parse.oldVars.id != vars.id) {
+		switch(vars.command) {
+			case 0:
+				console.log("Beginning playback");
+				spotify.play(callback);
+				break;
+			case 1:
+				break;
+			default:
+				console.log("Unknown command", vars.command);
+				break;
+		}
+	}
+	else if (parse.oldVars.volume != vars.volume) {
+		console.log("Setting volume to", vars.volume);
+		spotify.setVolume(vars.volume, callback);
+
+	}
+	parse.oldVars = vars;
+}
 
 
-
-
-// var spotify = require("spotify-node-applescript")
-// var colors = require("colors")
-// var https = require("https")
-//
-// var oldVars = {}
-// var interval = 1500 // Milliseconds between refresh
-// var projectID = "93077193"
-//
-// var spotifyMsg = ""
-// var spotifyCallback = function() {
-//   log("Spotify", spotifyMsg)
-// }
-//
-// // Logging
-// function error(err) {
-//   if (err) {
-//     console.log("[%s] %s", "Error".red.underline, err)
-//     return true
-//   }
-//   return false
-// }
-//
-// function log(event, msg) {
-//   console.log("[%s] %s", event.green.underline, msg)
-//
-// }
-//
-// function success(msg) {
-//   log("Success", msg)
-// }
-//
-// // Handle commands
-// function changeSpotifyPosition(change) {
-//   spotify.getState(function (err, state) {
-//     var pos = state["position"]
-//     spotifyMsg = "Track Jump: " + change
-//     spotify.jumpTo(pos + change, spotifyCallback)
-//   })
-// }
-//
-// function lamo() {
-//   require("child_process").exec("osascript ~/Desktop/Scratchify/easteregg.scpt", null)
-//   log("AppleScript", "Sent Message")
-// }
-//
-// function newCommand(command) {
-//   command = command * 1 // convert to int
-//   switch(command) {
-//     case 0:
-//       spotifyMsg = "Play"
-//       spotify.play(spotifyCallback)
-//       break
-//     case 1:
-//       changeSpotifyPosition(10)
-//       break
-//     case 2:
-//       spotifyMsg = "Next"
-//       spotify.next(spotifyCallback)
-//       break
-//     case 3:
-//       changeSpotifyPosition(-10)
-//       break
-//     case 4:
-//       spotifyMsg = "Previous"
-//       spotify.previous(spotifyCallback)
-//       break
-//     case 5:
-//       spotifyMsg = "Pause"
-//       spotify.pause(spotifyCallback)
-//       break
-//     case 6:
-//       lamo()
-//       break
-//   }
-// }
-//
-// function newVolume(volume) {
-//   spotifyMsg = "Set Volume: " + volume
-//   spotify.setVolume(volume, spotifyCallback)
-// }
-//
-// // Scratch API
-// function whatDoesThisMean(newVars) {
-//   if (oldVars["id"] != newVars["id"]) {
-//     newCommand(newVars["command"])
-//   }
-//   else if (oldVars["volume"] != newVars["volume"]) {
-//     newVolume(newVars["volume"])
-//   }
-//   oldVars = newVars
-// }
-//
-// function parseCloudHTML(body) {
-//   var result = {}
-//   try {
-//     var json = JSON.parse(body)["variables"]
-//     for (var object in json) {
-//       object = json[object]
-//       result[object["name"].substring(2)] = object["value"] // Substring to remove cloud icon
-//     }
-//   }
-//   catch(err) {
-//     error(err)
-//   }
-//   return result
-// }
-//
-// function refresh() {
-//   var opts = {
-//     host: "scratch.mit.edu",
-//     path: "/varserver/" + projectID
-//   }
-//   var request = https.get(opts, function (response) {
-//     var body = ""
-//     response.on("data", function(d) {
-//       body += d
-//     })
-//     response.on("end", function() {
-//       whatDoesThisMean(parseCloudHTML(body))
-//     })
-//   })
-//   request.on("error", function (err) {
-//     error(err)
-//   })
-// }
-//
-// success("Beginning refresh cycle with interval of " + interval + " milliseconds")
-// setInterval(refresh, 1000)
+/* Start listening */
+console.log("Client is ready.");
+spotify.isRunning(function(err, isRunning) {
+	if (isRunning) {
+		console.log("Spotify ready.");
+	}
+	else {
+		console.warn("Spotify is not running!");
+	}
+	console.log("Listening to project", projectID, "with", refreshInterval, "ms refresh interval.");
+	setInterval(refresh, refreshInterval);
+});
